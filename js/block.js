7823d8fa3588b99f1dca38a444e287cdfe54b0f1
juyoun kim <aa019@hotmail.com>
2020-12-31 16:50:20 +0900

    added basic block movement

--- js/block.js
+++ js/block.js
@@ -38,11 +38,47 @@ document.onmousemove = null;
 }
 
 
-const block = (className)=>{
+const block = (x, y, className)=>{
+    const text = `${x}-${y}`
     const div = document.createElement('div')
-    div.class = "className"
-    div.style.width = '50px'
-    div.style.height = '50px'
+    div.className = className
+    div.style.top = `${y*50}px`
+    div.style.left = `${x*50}px`
     div.style.backgroundColor = 'magenta'
+    div.innerText=text
+    div.dataset.x = x
+    div.dataset.y = y
+    div.onclick = blockOnClick
     return div
+}
+
+let selectedBlock = undefined
+const blockOnClick = (event) => {
+    console.log('event', event.target)
+    const x = event.target.dataset.x
+    const y = event.target.dataset.y
+    if (selectedBlock===undefined){
+        selectedBlock = array[x][y]
+        selectedBlock.style.border="2px solid yellow"
+    } else {
+        selectedBlock.style.border=""
+        const tempY = selectedBlock.dataset.y
+        const tempX = selectedBlock.dataset.x
+
+        selectedBlock.dataset.x = x
+        selectedBlock.dataset.y = y
+        
+        selectedBlock.style.top = y * 50 +"px"
+        selectedBlock.style.left = x * 50 +"px"
+        
+        array[tempX][tempY] = array[x][y]
+        array[tempX][tempY].dataset.x = tempX
+        array[tempX][tempY].dataset.y = tempY
+        array[tempX][tempY].style.top = tempY * 50 +"px"
+        array[tempX][tempY].style.left = tempX * 50 +"px"
+        
+        array[x][y] = selectedBlock
+        selectedBlock = undefined
+    }
+    console.log('end event', array)
 }
--- js/test.js
+++ js/test.js
@@ -1,1 +1,21 @@
-document.getElementById('game-board').appendChild(block('block-box'))
+let array = []
+let j = 0
+
+for (let i = 0 ; i < 9; i++){
+    array[i]=[]
+    for (let j = 0 ; j < 9; j++){
+        array[i][j] = block(i, j, 'block block-box')
+        document.getElementById('game-board').appendChild(array[i][j])
+    }
+}
+// document.getElementById('game-board').appendChild(block('block-box'))
+
+const reloadBoard = () => {
+    console.log('reloadBoard')
+    document.getElementById('game-board').innerHtml=""
+    for (let i = 0 ; i < array.length; i++){
+        for (let j = 0 ; j < array[i].length; j++){
+            document.getElementById('game-board').appendChild(array[i][j])
+        }
+    }
+}
