08b1ef6e9117fb5e6e4689addd46675c1cee42cd
juyoun kim <aa019@hotmail.com>
2021-01-01 21:38:12 +0900

    change test code to working prototype

--- js/block.js
+++ js/block.js
@@ -1,104 +1,108 @@
-// var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-// if (document.getElementById(elmnt.id + "header")) {
-// /* if present, the header is where you move the DIV from:*/
-// document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-// } else {
-// /* otherwise, move the DIV from anywhere inside the DIV:*/
-// elmnt.onmousedown = dragMouseDown;
+// // var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+// // if (document.getElementById(elmnt.id + "header")) {
+// // /* if present, the header is where you move the DIV from:*/
+// // document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+// // } else {
+// // /* otherwise, move the DIV from anywhere inside the DIV:*/
+// // elmnt.onmousedown = dragMouseDown;
+// // }
+
+// function dragMouseDown(e) {
+// e = e || window.event;
+// e.preventDefault();
+// // get the mouse cursor position at startup:
+// pos3 = e.clientX;
+// pos4 = e.clientY;
+// document.onmouseup = closeDragElement;
+// // call a function whenever the cursor moves:
+// document.onmousemove = elementDrag;
 // }
 
-function dragMouseDown(e) {
-e = e || window.event;
-e.preventDefault();
-// get the mouse cursor position at startup:
-pos3 = e.clientX;
-pos4 = e.clientY;
-document.onmouseup = closeDragElement;
-// call a function whenever the cursor moves:
-document.onmousemove = elementDrag;
-}
+// function elementDrag(e) {
+// e = e || window.event;
+// e.preventDefault();
+// // calculate the new cursor position:
+// pos1 = pos3 - e.clientX;
+// pos2 = pos4 - e.clientY;
+// pos3 = e.clientX;
+// pos4 = e.clientY;
+// // set the element's new position:
+// elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+// elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+// }
 
-function elementDrag(e) {
-e = e || window.event;
-e.preventDefault();
-// calculate the new cursor position:
-pos1 = pos3 - e.clientX;
-pos2 = pos4 - e.clientY;
-pos3 = e.clientX;
-pos4 = e.clientY;
-// set the element's new position:
-elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-}
+// function closeDragElement() {
+// /* stop moving when mouse button is released:*/
+// document.onmouseup = null;
+// document.onmousemove = null;
+// }
 
-function closeDragElement() {
-/* stop moving when mouse button is released:*/
-document.onmouseup = null;
-document.onmousemove = null;
-}
+// function getRandomInt(min, max) {
+//     min = Math.ceil(min)
+//     max = Math.floor(max)
+//     return Math.floor(Math.random() * (max - min)) + min
+// }
 
-function getRandomInt(min, max) {
-    min = Math.ceil(min)
-    max = Math.floor(max)
-    return Math.floor(Math.random() * (max - min)) + min
-}
+// const block = (x, y, className)=>{
+//     // const text = `${x}-${y}`
+//     const type = getRandomInt(1, 6)
+//     const div = document.createElement('div')
+//     div.className = className
+//     div.style.left = `${y*50}px`
+//     div.style.top = "-50px"//`${x*50}px`
+//     div.style.backgroundColor = 'magenta'
+//     div.innerText = type
+//     // div.dataset.x = x
+//     // div.dataset.y = y
+//     // div.dataset.type = type
+//     div.onclick = (event)=>blockOnClick(event, x,y)
+//     return {
+//         x,
+//         y,
+//         type,
+//         node: div
+//     }
+// }
 
-const block = (x, y, className)=>{
-    // const text = `${x}-${y}`
-    const type = getRandomInt(1, 6)
-    const div = document.createElement('div')
-    div.className = className
-    div.style.left = `${y*50}px`
-    div.style.top = `${x*50}px`
-    div.style.backgroundColor = 'magenta'
-    div.innerText = type
-    div.dataset.x = x
-    div.dataset.y = y
-    div.dataset.type = type
-    div.onclick = blockOnClick
-    return div
-}
+// let selectedBlock = undefined
+// const blockOnClick = (event, x,y) => {
+//     console.log('event', event.target)
+//     console.log('object',array[x][y])
+//     if (selectedBlock===undefined){
+//         selectedBlock = array[x][y]
+//         selectedBlock.node.style.border="2px solid yellow"
+//     } else {
+//         selectedBlock.node.style.border=""
+//         const tempY = selectedBlock.y
+//         const tempX = selectedBlock.x
 
-let selectedBlock = undefined
-const blockOnClick = (event) => {
-    console.log('event', event.target)
-    const x = event.target.dataset.x
-    const y = event.target.dataset.y
-    if (selectedBlock===undefined){
-        selectedBlock = array[x][y]
-        selectedBlock.style.border="2px solid yellow"
-    } else {
-        selectedBlock.style.border=""
-        const tempY = selectedBlock.dataset.y
-        const tempX = selectedBlock.dataset.x
-
-        selectedBlock.dataset.x = x
-        selectedBlock.dataset.y = y
+//         selectedBlock.x = x
+//         selectedBlock.y = y
         
-        selectedBlock.style.left = y * 50 +"px"
-        selectedBlock.style.top = x * 50 +"px"
+//         selectedBlock.node.style.left = y * 50 +"px"
+//         selectedBlock.node.style.top = x * 50 +"px"
         
-        array[tempX][tempY] = array[x][y]
-        array[tempX][tempY].dataset.x = tempX
-        array[tempX][tempY].dataset.y = tempY
-        array[tempX][tempY].style.left = tempY * 50 +"px"
-        array[tempX][tempY].style.top = tempX * 50 +"px"
+//         array[tempX][tempY] = array[x][y]
+//         array[tempX][tempY].x = tempX
+//         array[tempX][tempY].y = tempY
+//         array[tempX][tempY].node.style.left = tempY * 50 +"px"
+//         array[tempX][tempY].node.style.top = tempX * 50 +"px"
         
-        array[x][y] = selectedBlock
+//         array[x][y] = selectedBlock
 
-        array[tempX][tempY].style.zIndex = 1000
-        array[x][y].style.zIndex = 1000
-        setTimeout(()=>{
-            array[tempX][tempY].style.zIndex = ""
-            array[x][y].style.zIndex = ""
-            reloadBoard()
+//         array[tempX][tempY].node.style.zIndex = 1000
+//         array[x][y].node.style.zIndex = 1000
+//         setTimeout(()=>{
+//             array[tempX][tempY].node.style.zIndex = ""
+//             array[x][y].node.style.zIndex = ""
+//             reloadBoard()
             
-        },1100)
+//         },1100)
 
-        selectedBlock = undefined
-    }
-}
+//         selectedBlock = undefined
+//     }
+// }
 
-const moveBlock = () => {
+// const moveBlock = () => {
 
-}
+// }
---
+++ js/board.js
@@ -0,0 +1,202 @@
+// width
+const MaxX = 9
+
+// height
+const MaxY = 9
+
+let Board = []
+
+const Around = [
+    {x:-1, y: -1},
+    {x:0, y: -1},
+    {x:1, y: -1},
+    {x:-1, y: 0},
+    {x:1, y: 0},
+    {x:-1, y: 1},
+    {x:0, y: 1},
+    {x:1, y: 1},
+]
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min)) + min
+}
+
+// x -> bottom to top
+// y -> left to right
+
+const createBlock = (x,y, className) => {
+    // starting x is alway top
+    const type = getRandomInt(1, 6)
+    const div = document.createElement('div')
+    div.className = className
+    div.style.left = `${x*50}px`
+    div.style.top = `${-50*y}px`//`${x*50}px`
+    div.style.backgroundColor = 'magenta'
+    div.dataset.x = x
+    div.dataset.y = y
+    div.innerText = type
+    div.onclick = blockOnClick
+    document.getElementById('game-board').appendChild(div)
+    return {
+        x,
+        y,
+        type,
+        div,
+        deleteFlag:false,
+        // moveBlock
+    }
+}
+
+let selected = undefined
+const blockOnClick = async (event) => {
+    // const checkRes = await checkChain(Board[x][y])
+    // console.log('checkRes', checkRes)
+    const x = parseInt(event.target.dataset.x)
+    const y = parseInt(event.target.dataset.y)
+    console.log('event', event.target, this)
+    if (selected===undefined){
+        selected = Board[x][y]
+        selected.div.style.border="2px solid yellow"
+    } else {
+        selected.div.style.border=""
+        const temp = Board[x][y]
+        
+        const beforeY = selected.y
+        const beforeX = selected.x
+
+        temp.x = beforeX
+        temp.y = beforeY
+
+        Board[x][y] = selected
+        Board[x][y].x = x
+        Board[x][y].y = y
+
+        Board[beforeX][beforeY] = temp
+        
+        selected = undefined
+        await setBlocks()
+        setTimeout(()=>processing(),1400)
+    }
+}
+
+const setBlocks = () =>{
+    // loop array and set x,y with top left
+    return new Promise(resolve=>{
+        for (let x = 0 ; x < 9; x++){
+            for (let y = 8; y >= 0; y--){
+                Board[x][y].div.style.top = `${400-50*y}px`
+                Board[x][y].div.style.left = `${50*x}px`
+                Board[x][y].div.dataset.x = x
+                Board[x][y].x = x
+                Board[x][y].div.dataset.y = y
+                Board[x][y].y = y
+            }
+        }
+        resolve()
+    })
+}
+
+const checkBoard = (need2Run)=>new Promise(async resolve=>{
+    for (let i = 0 ; i < Board.length; i++){
+        for (let j = 0 ; j < Board[i].length; j++){
+            const check = Board[i][j] && await checkChain(Board[i][j])
+            if (check){
+                if (need2Run) {
+                    resolve(true)
+                }
+                Board[i][j].delete = "1"
+            }
+        }
+    }
+    resolve(false)
+})
+
+const removeBlocks = () =>{
+    return new Promise(resolve=>{
+        for (let i = 0 ; i < Board.length; i++){
+            let newArray = []
+            for (let j = 0 ; j < Board[i].length; j++){
+                if (Board[i][j]&&Board[i][j].delete==="1"){
+                    document.getElementById('game-board').removeChild(Board[i][j].div)
+                }else{
+                    newArray.push(Board[i][j])
+                }
+            }
+            Board[i] = newArray
+            for (let j= Board[i].length; j<9;j++){
+                Board[i].push(createBlock(i, j, 'block block-box'))
+            }
+        }
+        resolve()
+    })
+}
+
+const checkChain = async (self, direction) => {
+    const x = self.x
+    const y = self.y
+    
+    if (direction) {
+        const opposite = Around[direction]
+        const {x:diffX, y:diffY} = opposite
+        const checkX = (parseInt(x)+diffX)
+        const checkY = (parseInt(y)+diffY)
+        const checkObject = Board[checkX]&&Board[checkX][checkY]
+        return (checkObject && checkObject.type === self.type)
+    }else{
+        for (let i = 0 ; i < Around.length; i++){
+            const {x:diffX, y:diffY} = Around[i]
+            const checkX = (parseInt(x)+diffX)
+            const checkY = (parseInt(y)+diffY)
+            const checkObject = Board[checkX]&&Board[checkX][checkY]
+            if (checkObject && checkObject.type === self.type){
+                const opposite = Around[7-i]
+                const {x:diffX, y:diffY} = opposite
+                const checkX = (parseInt(x)+diffX)
+                const checkY = (parseInt(y)+diffY)
+                const oppositeObject = Board[checkX]&&Board[checkX][checkY]
+                if (oppositeObject && oppositeObject.type === self.type) {
+                    return true   
+                } else if (await checkChain(checkObject, i)===true){
+                    return true
+                }
+            }
+        }
+        return false
+    }   
+}
+
+const processing = async () =>{
+    await checkBoard()
+    await removeBlocks()
+    await setBlocks()
+    await new Promise(resolve=>{setTimeout(()=>resolve(),1500)})
+    const runProcess = await checkBoard(true)
+    if (runProcess) {
+        await processing()
+    }
+}
+
+for (let x = 0 ; x < 9; x++){
+    Board[x]=[]
+    for (let y = 8; y >= 0; y--){
+        Board[x][y] = createBlock(x, y, 'block block-box')
+        
+    }
+}
+
+setTimeout(async()=>{
+    await setBlocks()
+    await new Promise(resolve=>{setTimeout(()=>resolve(),1500)})
+    await processing()
+    // let processingCount = 0
+    // let runProcess = true
+    // while(runProcess){
+    //     runProcess = await checkBoard(true)
+    //     processingCount++
+    //     console.log('processingCount', processingCount)
+    //     await processing()
+    // }
+},1000)
+
